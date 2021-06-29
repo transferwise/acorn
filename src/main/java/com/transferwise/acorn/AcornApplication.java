d6f1bf0b7168f0f9e7ad65d5f00f9baf47c5b57e
Karl-Gustav Kallasmaa <karl-gustav.kallasmaa@transferwise.com>
2021-06-29 19:29:00 +0300

    initial commit

---
+++ src/main/java/com/transferwise/acorn/AcornApplication.java
@@ -0,0 +1,13 @@
+package com.transferwise.acorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcornApplication.class, args);
+	}
+
+}
