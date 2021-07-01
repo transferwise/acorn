ecc064f3246fea2b48e453bcd3a9fcf1642c4dbe
Abhirup Bose <abhirup.bose@transferwise.com>
2021-07-01 20:31:35 +0300

    refactor code

--- src/main/java/com/transferwise/acorn/services/BalanceService.java
+++ src/main/java/com/transferwise/acorn/services/BalanceService.java
@@ -1,9 +1,10 @@
 package com.transferwise.acorn.services;
 
-import com.transferwise.acorn.models.*;
 import com.transferwise.acorn.balance.InvalidBalanceException;
 import com.transferwise.acorn.models.BalanceCredit;
+import com.transferwise.acorn.models.BalanceTransferPayload;
 import com.transferwise.acorn.models.Icon;
+import com.transferwise.acorn.models.MoneyValue;
 import com.transferwise.acorn.models.OpenBalanceCommand;
 import com.transferwise.acorn.models.RuleDecision;
 import lombok.RequiredArgsConstructor;
@@ -14,13 +15,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class BalanceService {
 
     private static final String JAR_TYPE = "SAVINGS";
+    public static final String EMOJI_PINEAPPLE = "\uD83C\uDF4D";
     private final BalanceAPI balanceAPI;
     public static final String EMOJI = "EMOJI";
     public static final String SAVINGS_JAR_NAME_PREFIX = "SAVINGS ";
@@ -34,14 +35,11 @@ public class BalanceService {
             return;
         }
         final Long profileId = balanceCredit.getResource().getProfileId();
-        final var activeBalances = balanceAPI.findActiveBalances(token, profileId).orElseThrow(() -> new InvalidBalanceException("No Available Balances for your profile"));
+        final var activeBalances = balanceAPI.findActiveBalances(profileId);
 
         final String currency = balanceCredit.getCurrency();
         final Long sourceJarId = getSourceJarId(activeBalances, currency);
         final Long targetJarId = getTargetJarId(profileId, balanceCredit.getCurrency(), activeBalances);
-        if (targetJarId == null) {
-            return;
-        }
 
         BalanceTransferPayload balanceTransferPayload = BalanceTransferPayload.builder()
                 .amount(new MoneyValue(ruleDecision.getAmountToForwardToJar(), currency))
@@ -56,16 +54,15 @@ public class BalanceService {
         var predicates = getFilterPredicates(STANDARD_JAR_TYPE, currency);
         return balances.stream()
                 .filter(predicates.stream().reduce(x->true, Predicate::and))
-                .findFirst().map(it -> (Long.valueOf(it.id))).orElseThrow(() -> new InvalidBalanceException("No Regular Visible Balance Available for currency " + currency));
+                .findFirst().map(balance -> (Long.valueOf(balance.getId()))).orElseThrow(() -> new InvalidBalanceException("No Regular Visible Balance Available for currency " + currency));
     }
 
     private Long getTargetJarId(Long profileId, String currency, List<BalanceValue> balances) {
+        var predicates = getFilterPredicates(JAR_TYPE, currency);
         Optional<Long> currentTargetJarId = balances.stream()
-                .filter(openBalanceCommand -> openBalanceCommand.visible)
-                .filter(openBalanceCommand -> openBalanceCommand.currency.equals(currency))
-                .filter(openBalanceCommand -> JAR_TYPE.equals(openBalanceCommand.type))
-                .filter(openBalanceCommand -> openBalanceCommand.name.startsWith(SAVINGS_JAR_NAME_PREFIX))
-                .map(openBalanceCommand -> openBalanceCommand.id)
+                .filter(predicates.stream().reduce(x->true, Predicate::and))
+                .filter(balance -> balance.getName().startsWith(SAVINGS_JAR_NAME_PREFIX))
+                .map(balance -> balance.getId())
                 .findFirst();
         if (currentTargetJarId.isPresent()) {
             return currentTargetJarId.get();
@@ -75,24 +72,16 @@ public class BalanceService {
                 .currency(currency)
                 .type(JAR_TYPE)
                 .name(newName)
-                .icon(new Icon(EMOJI, "\uD83C\uDF4D"))
+                .icon(new Icon(EMOJI, EMOJI_PINEAPPLE))
                 .build();
 
-        final var newBalance = balanceAPI.createBalanceJar(profileId, token, command);
-        return newBalance.map(balanceValue -> Long.valueOf(balanceValue.id)).orElseThrow(() -> new InvalidBalanceException("New JAR could not be created"));
+        return balanceAPI.createBalanceJar(profileId, command).getId();
     }
 
     private List<Predicate<BalanceValue>> getFilterPredicates(String type, String currency) {
-        Predicate<BalanceValue> visibilityPredicate =  balance -> balance.visible;
+        Predicate<BalanceValue> visibilityPredicate =  balance -> balance.isVisible();
         Predicate<BalanceValue> currencyPredicate =  balance -> type.equals(balance.getType());
         Predicate<BalanceValue> typePredicate =   balance -> type.equals(balance.getType());
         return Arrays.asList(visibilityPredicate, currencyPredicate, typePredicate);
-    }
-
-    private Stream<BalanceValue> filterVisibleBalancesByCurrencyAndType1(List<BalanceValue> balances, String type, String currency) {
-        return balances.stream()
-                .filter(balance -> balance.visible)
-                .filter(balance -> type.equals(balance.getType()))
-                .filter(balance -> currency.equals(balance.getCurrency()));
     }
 }
