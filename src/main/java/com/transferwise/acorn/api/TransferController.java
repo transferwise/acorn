69f47b7775d96fed88c1d30761d4b0637eefef55
Abhirup Bose <abhirup.bose@transferwise.com>
2021-07-01 17:00:47 +0300

    refactoring into interface

--- src/main/java/com/transferwise/acorn/api/TransferController.java
+++ src/main/java/com/transferwise/acorn/api/TransferController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class TransferController {
     private final BalanceService transferService;
 
     @PostMapping("/transfer")
-    public ResponseEntity transfer(@RequestBody BalanceCreditEvent payload, HttpHeaders httpHeaders) {
+    public ResponseEntity transfer(@RequestBody BalanceCreditEvent payload, @RequestHeader HttpHeaders httpHeaders) {
         if (isRealBalanceEvent(payload, httpHeaders)) {
             transferService.handleIncomingDepositWebhooksEvent(payload.getData());
         }
